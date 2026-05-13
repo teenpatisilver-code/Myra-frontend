98fd415e07ffd1d6c5e703095973ee343a994790
teenpatisilver-code <teenpatisilver@gmail.com>
2026-05-13 14:11:13 +0545

    Create AdminApp.tsx

---
+++ src/admin/AdminApp.tsx
@@ -0,0 +1,32 @@
+import { Switch, Route } from 'wouter'
+import AdminLayout from './AdminLayout'
+import { useAdminGuard } from './hooks/useAdminGuard'
+import Dashboard from './pages/Dashboard'
+import Orders from './pages/Orders'
+import Drinks from './pages/Drinks'
+import Categories from './pages/Categories'
+import Customers from './pages/Customers'
+
+export default function AdminApp() {
+  const { loading, isAdmin } = useAdminGuard()
+
+  if (loading) return (
+    <div className="flex items-center justify-center h-screen bg-gray-950 text-white">
+      Loading...
+    </div>
+  )
+
+  if (!isAdmin) return null
+
+  return (
+    <AdminLayout>
+      <Switch>
+        <Route path="/admin" component={Dashboard} />
+        <Route path="/admin/orders" component={Orders} />
+        <Route path="/admin/drinks" component={Drinks} />
+        <Route path="/admin/categories" component={Categories} />
+        <Route path="/admin/customers" component={Customers} />
+      </Switch>
+    </AdminLayout>
+  )
+}
