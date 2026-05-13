175c56afae9928d1b89590925c307531e075bf07
teenpatisilver-code <teenpatisilver@gmail.com>
2026-05-13 09:35:32 +0545

    Update index.ts

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,4 +1,2 @@
-export * from "./generated/api.schemas";
-export * from "./generated/api";
-export { setBaseUrl, setAuthTokenGetter } from "./custom-fetch";
-export type { AuthTokenGetter } from "./custom-fetch";
+// Old API removed - now using Supabase
+export {}
