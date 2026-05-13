984cf19dd9c5dd6df4ec7f60a47f18f79ef4f333
teenpatisilver-code <teenpatisilver@gmail.com>
2026-05-13 10:30:44 +0545

    Update index.ts

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,14 +1,1 @@
-// Stub exports to prevent import errors while migrating to Supabase
-export const setBaseUrl = () => {}
-export const setAuthTokenGetter = () => {}
-
-export type AuthTokenGetter = () => string | Promise<string>
-
-// Re-export schemas if they exist
-try {
-  export * from "./generated/api.schemas"
-} catch (e) {
-  // Schemas don't exist yet
-}
-
 export {}
